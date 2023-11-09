9c26e6c49ec913862f3000824f8241c47fc5f9d3
André Cerutti Franciscatto <andre@franciscatto.com>
2023-11-09 14:01:03 -0300

    lslkdfsfs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify, request, session, url_for, re
 import uuid
 import sqlite3
 from utils import db_tricks
+import json
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
@@ -53,162 +54,139 @@ def offer():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 
-@app.route('/answer', methods=['POST'])
-def answer():
-    try:
-        data = request.get_json()
-        print('answer', data)
-        session_id = data['session_id']
-        answer_sdp = data['answer']
-        conn = sqlite3.connect('sessions.db')
-        c = conn.cursor()
-        # Inserindo ou atualizando a resposta para esta sessão
-        c.execute('''
-            INSERT INTO sessions (session_id, answer)
-            VALUES (?, ?)
-            ON CONFLICT(session_id) DO UPDATE SET answer = ?
-        ''', (session_id, answer_sdp, answer_sdp))
-        conn.commit()
-        conn.close()
-        return jsonify({'status': 'ok'})
-    except Exception as e:
-        print(f"Exception in answer: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
+# @app.route('/answer', methods=['POST'])
+# def answer():
+#     try:
+#         data = request.get_json()
+#         print('answer', data)
+#         session_id = data['session_id']
+#         answer_sdp = data['answer']
+#         conn = sqlite3.connect('sessions.db')
+#         c = conn.cursor()
+#         # Inserindo ou atualizando a resposta para esta sessão
+#         c.execute('''
+#             INSERT INTO sessions (session_id, answer)
+#             VALUES (?, ?)
+#             ON CONFLICT(session_id) DO UPDATE SET answer = ?
+#         ''', (session_id, answer_sdp, answer_sdp))
+#         conn.commit()
+#         conn.close()
+#         return jsonify({'status': 'ok'})
+#     except Exception as e:
+#         print(f"Exception in answer: {e}")
+#         return jsonify({'error': 'Internal Server Error'}), 500
+#
+
+#
+# @app.route('/get-offer/<session_id>', methods=['GET'])
+# def get_offer(session_id):
+#     try:
+#         print('get-offer', session_id)
+#         conn = sqlite3.connect('sessions.db')
+#         c = conn.cursor()
+#         c.execute('SELECT offer FROM sessions WHERE session_id = ?', (session_id,))
+#         row = c.fetchone()
+#         conn.close()
+#         if row:
+#             return jsonify({'offer': row[0]})
+#         return jsonify({'error': 'Offer not found'}), 404
+#     except Exception as e:
+#         print(f"Exception in get_offer: {e}")
+#         return jsonify({'error': 'Internal Server Error'}), 500
+
+
+# @app.route('/get-answer/<session_id>', methods=['GET'])
+# def get_answer(session_id):
+#     try:
+#         print('get-answer', session_id)
+#         conn = sqlite3.connect('sessions.db')
+#         c = conn.cursor()
+#         c.execute('SELECT answer FROM sessions WHERE session_id = ?', (session_id,))
+#         row = c.fetchone()
+#         conn.close()
+#         if row:
+#             return jsonify({'answer': row[0]})
+#         return jsonify({'error': 'Answer not found'}), 404
+#     except Exception as e:
+#         print(f"Exception in get_answer: {e}")
+#         return jsonify({'error': 'Internal Server Error'}), 500
 
 
 
-@app.route('/get-offer/<session_id>', methods=['GET'])
-def get_offer(session_id):
-    try:
-        print('get-offer', session_id)
-        conn = sqlite3.connect('sessions.db')
-        c = conn.cursor()
-        c.execute('SELECT offer FROM sessions WHERE session_id = ?', (session_id,))
-        row = c.fetchone()
-        conn.close()
-        if row:
-            return jsonify({'offer': row[0]})
-        return jsonify({'error': 'Offer not found'}), 404
-    except Exception as e:
-        print(f"Exception in get_offer: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-
-@app.route('/get-answer/<session_id>', methods=['GET'])
-def get_answer(session_id):
-    try:
-        print('get-answer', session_id)
-        conn = sqlite3.connect('sessions.db')
-        c = conn.cursor()
-        c.execute('SELECT answer FROM sessions WHERE session_id = ?', (session_id,))
-        row = c.fetchone()
-        conn.close()
-        if row:
-            return jsonify({'answer': row[0]})
-        return jsonify({'error': 'Answer not found'}), 404
-    except Exception as e:
-        print(f"Exception in get_answer: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-
-import json
-
-@app.route('/ice-candidate', methods=['POST'])
-def ice_candidate():
-    try:
-        data = request.get_json()
-        print('ice-candidate', data)
-        session_id = data['session_id']
-        candidate = json.dumps(data['candidate'])  # Serializa o objeto candidate para uma string JSON
-        conn = sqlite3.connect('sessions.db')
-        c = conn.cursor()
-        c.execute('''
-            INSERT INTO candidates (session_id, candidate)
-            VALUES (?, ?)
-        ''', (session_id, candidate))
-        conn.commit()
-        conn.close()
-        return jsonify({'status': 'ok'})
-    except Exception as e:
-        print(f"Exception in ice_candidate: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-
-
-
-@app.route('/get-candidates/<session_id>', methods=['GET'])
-def get_candidates(session_id):
-    try:
-        conn = sqlite3.connect('sessions.db')
-        c = conn.cursor()
-        c.execute('SELECT candidate FROM candidates WHERE session_id = ?', (session_id,))
-        # Aqui, em vez de retornar uma lista de strings JSON,
-        # deserializamos cada candidato para um objeto Python e retornamos a lista desses objetos.
-        candidates = [json.loads(row[0]) for row in c.fetchall()]
-        conn.close()
-        if candidates:
-            return jsonify({'candidates': candidates})
-        return jsonify({'error': 'Candidates not found'}), 404
-    except Exception as e:
-        print(f"Exception in get_candidates: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-
-@app.route('/ice-candidate', methods=['POST'])
-def save_ice_candidate():
-    try:
-        data = request.get_json()
-        session_id = data['session_id']
-        ice_candidate = json.dumps(data['candidate'])
-
-        with sqlite3.connect('sessions.db') as conn:
-            c = conn.cursor()
-            # Recuperar os candidatos ICE atuais, adicionar o novo e salvar novamente
-            c.execute('SELECT ice_candidates FROM signaling WHERE session_id = ?', (session_id,))
-            row = c.fetchone()
-            candidates = json.loads(row[0]) if row and row[0] else []
-            candidates.append(ice_candidate)
-            c.execute('''
-                UPDATE signaling SET ice_candidates = ? WHERE session_id = ?
-            ''', (json.dumps(candidates), session_id))
-            conn.commit()
-
-        return jsonify({'status': 'ok'})
-    except Exception as e:
-        print(f"Exception in save_ice_candidate: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-@app.route('/get-sdp/<session_id>', methods=['GET'])
-def get_sdp(session_id):
-    try:
-        with sqlite3.connect('sessions.db') as conn:
-            c = conn.cursor()
-            c.execute('SELECT offer, answer FROM signaling WHERE session_id = ?', (session_id,))
-            row = c.fetchone()
-
-        if row:
-            return jsonify({'offer': row[0], 'answer': row[1]})
-        else:
-            return jsonify({'error': 'Session not found'}), 404
-    except Exception as e:
-        print(f"Exception in get_sdp: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-@app.route('/get-ice-candidates/<session_id>', methods=['GET'])
-def get_ice_candidates(session_id):
-    try:
-        with sqlite3.connect('sessions.db') as conn:
-            c = conn.cursor()
-            c.execute('SELECT ice_candidates FROM signaling WHERE session_id = ?', (session_id,))
-            row = c.fetchone()
-
-        if row and row[0]:
-            return jsonify({'ice_candidates': json.loads(row[0])})
-        else:
-            return jsonify({'error': 'No ICE candidates found'}), 404
-    except Exception as e:
-        print(f"Exception in get_ice_candidates: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
+#
+# @app.route('/get-candidates/<session_id>', methods=['GET'])
+# def get_candidates(session_id):
+#     try:
+#         conn = sqlite3.connect('sessions.db')
+#         c = conn.cursor()
+#         c.execute('SELECT candidate FROM candidates WHERE session_id = ?', (session_id,))
+#         # Aqui, em vez de retornar uma lista de strings JSON,
+#         # deserializamos cada candidato para um objeto Python e retornamos a lista desses objetos.
+#         candidates = [json.loads(row[0]) for row in c.fetchall()]
+#         conn.close()
+#         if candidates:
+#             return jsonify({'candidates': candidates})
+#         return jsonify({'error': 'Candidates not found'}), 404
+#     except Exception as e:
+#         print(f"Exception in get_candidates: {e}")
+#         return jsonify({'error': 'Internal Server Error'}), 500
+#
+#
+# @app.route('/ice-candidate', methods=['POST'])
+# def save_ice_candidate():
+#     try:
+#         data = request.get_json()
+#         session_id = data['session_id']
+#         ice_candidate = json.dumps(data['candidate'])
+#
+#         with sqlite3.connect('sessions.db') as conn:
+#             c = conn.cursor()
+#             # Recuperar os candidatos ICE atuais, adicionar o novo e salvar novamente
+#             c.execute('SELECT ice_candidates FROM signaling WHERE session_id = ?', (session_id,))
+#             row = c.fetchone()
+#             candidates = json.loads(row[0]) if row and row[0] else []
+#             candidates.append(ice_candidate)
+#             c.execute('''
+#                 UPDATE signaling SET ice_candidates = ? WHERE session_id = ?
+#             ''', (json.dumps(candidates), session_id))
+#             conn.commit()
+#
+#         return jsonify({'status': 'ok'})
+#     except Exception as e:
+#         print(f"Exception in save_ice_candidate: {e}")
+#         return jsonify({'error': 'Internal Server Error'}), 500
+#
+# @app.route('/get-sdp/<session_id>', methods=['GET'])
+# def get_sdp(session_id):
+#     try:
+#         with sqlite3.connect('sessions.db') as conn:
+#             c = conn.cursor()
+#             c.execute('SELECT offer, answer FROM signaling WHERE session_id = ?', (session_id,))
+#             row = c.fetchone()
+#
+#         if row:
+#             return jsonify({'offer': row[0], 'answer': row[1]})
+#         else:
+#             return jsonify({'error': 'Session not found'}), 404
+#     except Exception as e:
+#         print(f"Exception in get_sdp: {e}")
+#         return jsonify({'error': 'Internal Server Error'}), 500
+#
+# @app.route('/get-ice-candidates/<session_id>', methods=['GET'])
+# def get_ice_candidates(session_id):
+#     try:
+#         with sqlite3.connect('sessions.db') as conn:
+#             c = conn.cursor()
+#             c.execute('SELECT ice_candidates FROM signaling WHERE session_id = ?', (session_id,))
+#             row = c.fetchone()
+#
+#         if row and row[0]:
+#             return jsonify({'ice_candidates': json.loads(row[0])})
+#         else:
+#             return jsonify({'error': 'No ICE candidates found'}), 404
+#     except Exception as e:
+#         print(f"Exception in get_ice_candidates: {e}")
+#         return jsonify({'error': 'Internal Server Error'}), 500
 
 @app.route('/request_access', methods=['GET', 'POST'])
 def index():
@@ -309,6 +287,92 @@ def enter_doctor_room():  # Função quando o paciente iniciará o processo de v
         criteria=session['session_id']
     )
     return render_template('enter_doctor_room.html', answer=row[1])
+
+
+
+
+@app.route('/ice-candidate', methods=['POST'])
+def ice_candidate():
+    try:
+        data = request.get_json()
+        session_id = data['session_id']
+        candidate = json.dumps(data['candidate'])  # Serializa o objeto candidate para uma string JSON
+
+        num_rows = db_tricks.calculate_table_size(
+            db_file='sessions.db',
+            db='client_ice_candidates'
+        )
+
+        if num_rows is None or num_rows == '':
+            idd = 1
+        else:
+            idd = int(num_rows) + 1
+
+        dic = {
+            'id': idd,
+            'session_id': session_id,
+            'candidate': candidate
+        }
+
+        db_tricks.add_entry(
+            db_file='sessions.db',
+            db='client_ice_candidates',
+            dic=dic
+        )
+
+        return jsonify({'status': 'ok'})
+    except Exception as e:
+        print(f"Exception in ice_candidate: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+
+@app.route('/ice-candidate-doctor', methods=['POST'])
+def ice_candidate_doctor():
+    try:
+        data = request.get_json()
+        session_id = data['session_id']
+        candidate = json.dumps(data['candidate'])  # Serializa o objeto candidate para uma string JSON
+
+        num_rows = db_tricks.calculate_table_size(
+            db_file='sessions.db',
+            db='doctor_ice_candidates'
+        )
+
+        if num_rows is None or num_rows == '':
+            idd = 1
+        else:
+            idd = num_rows + 1
+
+        dic = {
+            'id': idd,
+            'session_id': session_id,
+            'candidate': candidate
+        }
+
+        db_tricks.add_entry(
+            db_file='sessions.db',
+            db='doctor_ice_candidates',
+            dic=dic
+        )
+
+        return jsonify({'status': 'ok'})
+    except Exception as e:
+        print(f"Exception in ice_candidate_doctor: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+@app.route('/get-ice-candidate-from-doctor', methods=['GET', 'POST'])
+def get_ice_candidates_from_doctor():
+    session_id = request.args.get('session_id')
+
+    candidates = db_tricks.search_entire_db(
+        db_file='sessions.db',
+        db='doctor_ice_candidates',
+        field='session_id',
+        criteria=session_id
+    )
+    candidate_list = list(candidates)
+
+    return jsonify(candidate_list)
 
 
 if __name__ == '__main__':
